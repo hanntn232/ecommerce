29439d43cd4960df7e80a7e4b81a7a33ecd6146e
hanntn232 <91482056+hanntn232@users.noreply.github.com>
2024-07-17 20:32:35 +0700

    feat(register): create UI for register page

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import App from "./../../App";
 import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 const Register = () => {
   return (
@@ -69,6 +70,27 @@ const Register = () => {
                   Sign in
                 </Link>
               </p>
+            </div>
+            <div className="w-full flex justify-center items-center mb-3">
+              <div className="w-[45%] bg-slate-700 h-[1px]"></div>
+              <div className="w-[10%] flex justify-center items-center">
+                <span className="pb-1">Or</span>
+              </div>
+              <div className="w-[45%] bg-slate-700 h-[1px]"></div>
+            </div>
+
+            <div className="flex justify-center items-center gap-3">
+              <div className="w-[135px] h-[35px] flex rounded-md bg-orange-700 shadow-lg hover:shadow-orange-700/50 cursor-pointer items-center overflow-hidden justify-center">
+                <span>
+                  <FaGoogle />
+                </span>
+              </div>
+
+              <div className="w-[135px] h-[35px] flex rounded-md bg-blue-700 shadow-lg hover:shadow-blue-700/50 cursor-pointer items-center overflow-hidden justify-center">
+                <span>
+                  <FaFacebook />
+                </span>
+              </div>
             </div>
           </form>
         </div>
