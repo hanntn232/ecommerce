8f912a2605138822e43341fc2a2e513ded8ca787
hanntn232 <91482056+hanntn232@users.noreply.github.com>
2024-07-22 17:38:00 +0700

    feat(admin): create UI and initialize redux store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React, { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-const App = lazy(()=> import('./App'))
+import { Provider } from 'react-redux';
+import store from './store/index'
+import { Toaster } from 'react-hot-toast';
+const App = lazy(() => import('./App'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <Suspense>
+        <App />
+        <Toaster toastOptions={{
+          position: 'top-right',
+          style: {
+            background: '#283046',
+            color: 'white'
+          }
+        }} />
+      </Suspense>
+    </Provider>
   </BrowserRouter>
 );
 
