9e0b1769ae394746a5f1e67d8e23aba86bdb6920
hanntn232 <91482056+hanntn232@users.noreply.github.com>
2024-07-27 00:44:02 +0700

    feat(db): connect to mongoDB

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,10 +1,17 @@
+const adminModel = require('../models/adminModel')
+const { responseReture } = require('../utiles/response')
 class authControllers {
-    admin_login = async(req, res) => {
-        const {email, password} = req.body;
+    admin_login = async (req, res) => {
+        const { email, password } = req.body;
         try {
+            const admin = await adminModel.findOne({ email }).select('+password')
+            if (admin) {
 
-        } catch(error) {
-
+            } else {
+                responseReture(res, 404, { error: "Email not found" })
+            }
+        } catch (error) {
+            responseReture(res, 500, { error: error.message })
         }
     }
 }
