3e07a661014f4a1678c8972783f58ba109a1ebbf
hanntn232 <91482056+hanntn232@users.noreply.github.com>
2024-07-27 00:12:01 +0700

    feat(server): setup server

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,12 @@
+class authControllers {
+    admin_login = async(req, res) => {
+        const {email, password} = req.body;
+        try {
+
+        } catch(error) {
+
+        }
+    }
+}
+
+module.exports = new authControllers();
