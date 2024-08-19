5d243c51a5d526d3202a7ac22e4a1b6139f52101
hanntn232 <91482056+hanntn232@users.noreply.github.com>
2024-08-19 16:06:35 +0700

    feat(admin-login): add error and success notification

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,17 +1,30 @@
 const adminModel = require('../models/adminModel')
-const { responseReture } = require('../utiles/response')
+const { responseReturn } = require('../utiles/response')
+const bcrpty = require('bcrypt')
+const { createToken } = require('../utiles/tokenCreate')
 class authControllers {
     admin_login = async (req, res) => {
         const { email, password } = req.body;
         try {
             const admin = await adminModel.findOne({ email }).select('+password')
             if (admin) {
+                const match = await bcrpty.compare(password, admin.password);
 
+                if (match) {
+                    const token = await createToken({
+                        id: admin.id,
+                        role: admin.role
+                    })
+                    res.cookie('accessToken', token, { expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) })
+                    responseReturn(res, 200, { token, message: "Login success"})
+                } else {
+                    responseReturn(res, 404, { error: "Password wrong" })
+                }
             } else {
-                responseReture(res, 404, { error: "Email not found" })
+                responseReturn(res, 404, { error: "Email not found" })
             }
         } catch (error) {
-            responseReture(res, 500, { error: error.message })
+            responseReturn(res, 500, { error: error.message })
         }
     }
 }
